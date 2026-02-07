4e201918511ac301ed601fc16f1649ddf3ef80e7
Lepisma <hugo.diaz.morcillo@gmail.com>
2026-02-07 22:23:21 +0100

    better panels

--- public/ac_panel_script.js
+++ public/ac_panel_script.js
@@ -3,8 +3,25 @@ InitPanels();
 function InitPanels() {
     let panels = document.querySelectorAll('.ac-panel');
     panels.forEach(panel => {
+        // Deactivate panel
         SetPanelActive(panel, false);
         AnimatePanelTowards(panel, false);
+
+        // Set button actions
+        let buttons = panel.querySelectorAll(":scope > button");
+        buttons.forEach(button => {
+            let subpanel = button.getAttribute('ac_goto');
+            let panelID = `${panel.id}-${subpanel}`;
+
+            if (subpanel == "back") {
+                let idx = panel.id.lastIndexOf('-');
+                panelID = panel.id.slice(0, idx);
+            }
+
+            let func = `SwitchPanel('${panelID}')`;
+            button.setAttribute('onclick', func);
+            button.removeAttribute('ac_goto');
+        })
     });
 
     let active = document.querySelector('.ac-begin');
@@ -13,25 +30,19 @@ function InitPanels() {
 
 function SetPanelActive(panel, active) {
     if (active) {
+        // Activate panel, no animations
         panel.classList.remove('ac-inactive');
         panel.classList.remove('ac-move-right');
         panel.classList.remove('ac-move-left');
-
-        requestAnimationFrame(() => {
-            panel.classList.add('ac-active');
-        });
+        panel.classList.add('ac-active');
     } else {
+        // Deactivate panel
         panel.classList.remove('ac-active');
-
-
-        requestAnimationFrame(() => {
-            panel.classList.add('ac-inactive');
-        });
+        panel.classList.add('ac-inactive');
     }
 }
 
 function AnimatePanelTowards(panel, right) {
-    console.debug(right);
     if (right) {
         panel.classList.remove('ac-move-left');
         panel.classList.add('ac-move-right');
@@ -69,4 +80,3 @@ function SwitchPanel(newPanel) {
     let isNewDeeper = IsDeeperThan(active, inactive);
     AnimatePanelTowards(inactive, isNewDeeper);
 }
-
