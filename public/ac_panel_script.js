560a6d79c008427abf9e6ff6a10182d57240e568
Lepisma <hugo.diaz.morcillo@gmail.com>
2026-02-08 01:36:03 +0100

    lines

--- public/ac_panel_script.js
+++ public/ac_panel_script.js
@@ -1,4 +1,8 @@
+const svg = document.getElementById("lines");
+const lines = [];
+
 InitPanels();
+Animate();
 
 function InitPanels() {
     let panels = document.querySelectorAll('.ac-panel');
@@ -16,6 +20,11 @@ function InitPanels() {
             if (subpanel == "back") {
                 let idx = panel.id.lastIndexOf('-');
                 panelID = panel.id.slice(0, idx);
+
+            } else {
+                let other = document.getElementById(panelID);
+                CreateLine(other, button);
+                button.setAttribute('ac_tgt', panelID);
             }
 
             let func = `SwitchPanel('${panelID}')`;
@@ -76,7 +85,28 @@ function AnimatePanelTowards(panel, right, animateParent) {
     }
 }
 
+function CreateLinesFor(panel) {
+    RemoveAllLines();
+
+    let lastPanel = panel;
+
+    while (true) {
+        let parent = GetPanelParent(lastPanel);
+        if (parent == null) break;
+
+        let selector = `:scope > button[ac_tgt*="${lastPanel.id}"]`;
+        let from = lastPanel.querySelector('.ac_panel_header');
+        let to = parent.querySelector(selector);
+        CreateLine(from, to);
+
+        lastPanel = parent;
+    }
+    return;
+}
+
 function GetPanelParent(panel) {
+    if (panel.id.indexOf('-') == -1) return null;
+
     let idx = panel.id.lastIndexOf('-');
     let id = panel.id.slice(0, idx);
     return document.getElementById(id);
@@ -109,4 +139,54 @@ function SwitchPanel(newPanel) {
 
     let isNewDeeper = IsDeeperThan(active, inactive);
     AnimatePanelTowards(inactive, isNewDeeper, true);
+
+    CreateLinesFor(active);
+}
+
+function CreateLine(a, b) {
+    const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    svg.appendChild(path);
+
+    const line = { boxA: a, boxB: b, path };
+    lines.push(line);
+
+    return line;
+}
+
+function RemoveLine(line) {
+    svg.removeChild(line.path);
+
+    const index = lines.indexOf(line);
+    if (index > -1) {
+        lines.splice(index, 1);
+    }
+}
+
+function RemoveAllLines() {
+    lines.forEach(line => svg.removeChild(line.path));
+    lines.length = 0;
+}
+
+function UpdateLines() {
+    lines.forEach(line => {
+        const b1 = line.boxA.getBoundingClientRect();
+        const b2 = line.boxB.getBoundingClientRect();
+        const c = svg.getBoundingClientRect();
+
+        const x1 = b1.right - c.left;
+        const y1 = b1.top + b1.height / 2 - c.top;
+
+        const x2 = b2.left - c.left;
+        const y2 = b2.top + b2.height / 2 - c.top;
+
+        const midX = (x1 + x2) / 2;
+
+        const d = `M ${x1} ${y1} L ${midX} ${y1} L ${midX} ${y2} L ${x2} ${y2}`;
+        line.path.setAttribute("d", d);
+    });
+}
+
+function Animate() {
+    UpdateLines();
+    requestAnimationFrame(Animate);
 }
