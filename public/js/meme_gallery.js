d74a7ffef8b1ede1ac2319cc253aa1b0275ad927
Lepisma <hugo.diaz.morcillo@gmail.com>
2026-02-09 02:50:00 +0100

    css

--- public/js/meme_gallery.js
+++ public/js/meme_gallery.js
@@ -1,18 +1,28 @@
 ImportMemes();
+let memes = null;
+let currentMeme = 0;
 
 async function ImportMemes() {
     const memeImporter = await import("./meme_importer.js");
-    const memes = memeImporter.GetMemeData();
-
-    memes.files.forEach(meme => {
-        //DisplayMeme(meme);
-    });
+    memes = memeImporter.GetMemeData().files;
 }
 
-function DisplayMeme(url) {
-    let src = 'https://lepsima.github.io/Neocities/assets/' + url;
-    let img = document.createElement('img');
+function GetRandomMeme() {
+    const random = Math.floor(Math.random() * memes.length);
+    currentMeme = random;
+    return GetSource(memes[currentMeme]);
+}
 
-    img.src = src;
-    document.body.appendChild(img);
+function GetNextMeme(dir) {
+    if (dir) currentMeme++;
+    else currentMeme--;
+
+    if (currentMeme < 0) currentMeme = memes.length - 1;
+    if (currentMeme >= memes.length) currentMeme = 0;
+
+    return GetSource(memes[currentMeme]);
+}
+
+function GetSource(url) {
+    return 'https://lepsima.github.io/Neocities/assets/' + url;
 }
