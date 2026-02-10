57fd63a58f39245a82db9118850173be4fd337af
Lepisma <hugo.diaz.morcillo@gmail.com>
2026-02-10 15:47:11 +0100

    asd

--- public/js/title_randomizer.js
+++ public/js/title_randomizer.js
@@ -5,3 +5,12 @@ let titles = [
 const titleelement = document.getElementById("randomTitle");
 const randomTitle = Math.floor(Math.random() * titles.length);
 titleelement.innerHTML = titles[randomTitle];
+
+
+// discord, bsky, mail
+
+// github, gitlab, youtube, itchio, mail
+
+// steam, mail
+
+// github, gitlab, youtube, discord, bsky, steam, itch io, mail
